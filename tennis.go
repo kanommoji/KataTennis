78f12d7e6ba5be7d57c0f981664cbf16347546a0
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 15:01:28 +0700

    [Edited]: แยก code แสดงคะแนนออกมาเป็น showScore ใน tennis.go

--- tennis.go
+++ tennis.go
@@ -1,11 +1,10 @@
 package tennis
 
 func SummaryTennisGame(scorePlayerOne int, scorePlayerTwo int) string {
-	scores := [4]string{"LOVE","15","30","40"}
 	if scorePlayerOne == 4 || scorePlayerTwo == 4{
 		return checkWinner(scorePlayerOne,scorePlayerTwo)
 	}
-	return scores[scorePlayerOne]+" - "+scores[scorePlayerTwo]
+	return showScore(scorePlayerOne,scorePlayerTwo)
 }
 
 func checkWinner(scorePlayerOne int, scorePlayerTwo int) string {
@@ -13,5 +12,9 @@ func checkWinner(scorePlayerOne int, scorePlayerTwo int) string {
 		return "Player 1 Win"
 	}
 	return "Player 2 Win"
-	
+}
+
+func showScore(scorePlayerOne int, scorePlayerTwo int) string{
+	scores := [4]string{"LOVE","15","30","40"}
+	return scores[scorePlayerOne]+" - "+scores[scorePlayerTwo]
 }

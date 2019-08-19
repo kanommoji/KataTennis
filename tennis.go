8ddc046cdb88b4363c24f11adab2ae5452929522
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 14:25:51 +0700

    [Added]: ใส่เงื่อนไขผู้ชนะ ใน tennis.go

--- tennis.go
+++ tennis.go
@@ -1,8 +1,14 @@
 package tennis
 
 func SummaryTennisGame(scorePlayerOne int, scorePlayerTwo int) string {
-	score := [5]string{"LOVE","15","30","40"}
+	score := [5]string{"LOVE","15","30","40","Win"}
 	playerOne := score[scorePlayerOne]
 	playerTwo := score[scorePlayerTwo]
+	if playerOne == score[4] {
+		return "Player 1 Win"
+	}
+	if playerTwo == score[4] {
+		return "Player 2 Win"
+	}
 	return playerOne+" - "+playerTwo
 }

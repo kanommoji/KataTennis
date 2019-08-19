f64fcd54b2516743c483a729550bed3b7c591c5f
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 14:18:08 +0700

    [Added]: ใส่คะแนน 40 ใน array ใน tennis.go

--- tennis.go
+++ tennis.go
@@ -1,7 +1,7 @@
 package tennis
 
 func SummaryTennisGame(scorePlayerOne int, scorePlayerTwo int) string {
-	score := [5]string{"LOVE","15","30"}
+	score := [5]string{"LOVE","15","30","40"}
 	playerOne := score[scorePlayerOne]
 	playerTwo := score[scorePlayerTwo]
 	return playerOne+" - "+playerTwo

beeac61955ca58f849141f85a4e57f8b94522964
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 14:57:51 +0700

    [Edited]: แยกคำสั่งตรวจสอบผู้ชนะเป็นฟังก์ชัน checkWinner ใน tennis.go

--- tennis.go
+++ tennis.go
@@ -1,12 +1,17 @@
 package tennis
 
 func SummaryTennisGame(scorePlayerOne int, scorePlayerTwo int) string {
-	score := [5]string{"LOVE","15","30","40","Win"}
+	scores := [4]string{"LOVE","15","30","40"}
+	if scorePlayerOne == 4 || scorePlayerTwo == 4{
+		return checkWinner(scorePlayerOne,scorePlayerTwo)
+	}
+	return scores[scorePlayerOne]+" - "+scores[scorePlayerTwo]
+}
+
+func checkWinner(scorePlayerOne int, scorePlayerTwo int) string {
 	if scorePlayerOne == 4 {
 		return "Player 1 Win"
 	}
-	if scorePlayerTwo == 4 {
-		return "Player 2 Win"
-	}
-	return score[scorePlayerOne]+" - "+score[scorePlayerTwo]
+	return "Player 2 Win"
+	
 }

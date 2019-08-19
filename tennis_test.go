8f5742f25738f679bcfe7c93a138437d18b506f2
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 14:12:25 +0700

    [Added]: unit test กรณีคะแนน 15-0 ใน tennis_test.go

--- tennis_test.go
+++ tennis_test.go
@@ -1,13 +1,28 @@
 package tennis_test
 
-import "testing"
+import (
+	"tennis"
+	"testing"
+)
 
 func Test_SummaryTennisGame_Input_Score_Player_One_0_Player_Two_0_Should_Be_LOVE_LOVE(t *testing.T) {
 	expected := "LOVE - LOVE"
 	scorePlayerOne := 0
 	scorePlayerTwo := 0
 
-	actual := SummaryTennisGame(scorePlayerOne, scorePlayerTwo)
+	actual := tennis.SummaryTennisGame(scorePlayerOne, scorePlayerTwo)
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
+
+func Test_SummaryTennisGame_Input_Score_Player_One_1_Player_Two_0_Should_Be_15_LOVE(t *testing.T) {
+	expected := "15 - LOVE"
+	scorePlayerOne := 1
+	scorePlayerTwo := 0
+
+	actual := tennis.SummaryTennisGame(scorePlayerOne, scorePlayerTwo)
 
 	if expected != actual {
 		t.Errorf("Expect %s but got %s", expected, actual)

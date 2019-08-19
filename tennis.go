ed0a20858a94aa20151c045d7c8c2325a4b258aa
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 14:31:06 +0700

    [Edited]: ลดการใช้ตัวแปรที่ไม่จำเป็น ใน tennis.go

--- tennis.go
+++ tennis.go
@@ -2,13 +2,11 @@ package tennis
 
 func SummaryTennisGame(scorePlayerOne int, scorePlayerTwo int) string {
 	score := [5]string{"LOVE","15","30","40","Win"}
-	playerOne := score[scorePlayerOne]
-	playerTwo := score[scorePlayerTwo]
-	if playerOne == score[4] {
+	if scorePlayerOne == 4 {
 		return "Player 1 Win"
 	}
-	if playerTwo == score[4] {
+	if scorePlayerTwo == 4 {
 		return "Player 2 Win"
 	}
-	return playerOne+" - "+playerTwo
+	return score[scorePlayerOne]+" - "+score[scorePlayerTwo]
 }
